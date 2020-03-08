9e9543a2cc5ad1ba1e17a9d5b5e8b64a6dd19f1e
Alan Barcellos <zzzsoldierbrzzz@gmail.com>
2020-03-08 14:03:58 -0300

    atualizando o script

--- curlYoutube.php
+++ curlYoutube.php
@@ -15,28 +15,10 @@ class curlYoutube {
   
   public function exec_curl($nome) {
     $result = curl_exec($this->set_curl($nome));
-    preg_match_all('/\d{1,3}.\d{1,3}.\d{1,3} visualizações/m', $result, $videos_views, PREG_SET_ORDER);
     preg_match_all('/watch[?]v=.{11}/m', $result, $links, PREG_SET_ORDER);
-    $videos_views = $this->reOrder($videos_views);
     $links = $this->reOrder($links);
     $links = $this->removeDuplicateValue($links);
-    $views[] = (int) str_replace('.', '', str_replace('visualizações', '', $videos_views[0]));
-    $views[] = (int) str_replace('.', '', str_replace('visualizações', '', $videos_views[1]));
-    $views[] = (int) str_replace('.', '', str_replace('visualizações', '', $videos_views[2]));
-    $views = array_keys($this->select($views));
-    $pos = $views[0];
-    $video = $links[$pos];
-    return 'https://www.youtube.com/'.$video;
-  }
-  private function select($v) {
-    if ($v[0] > $v[1] && $v[2]) {
-      $maior[0] = $v[0];
-    } elseif ($v[1] > $v[0] && $v[2]) {
-      $maior[1] = $v[1];
-    } elseif ($v[2] > $v[0] && $v[1]) {
-      $maior[2] = $v[2];
-    }
-    return $maior;
+    return 'https://www.youtube.com/'.$links;
   }
   private function reOrder($vetor) {
     foreach ($vetor as $in => $val) {
@@ -53,11 +35,7 @@ class curlYoutube {
       }
     }
     $vetor = array_values($vetor);
-    return [
-      $vetor[0],
-      $vetor[1],
-      $vetor[2]
-    ];
+    return $vetor[0];
   }
 }
 ?>
